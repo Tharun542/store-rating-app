8b99b70f8f488956279e7f1b16d8d9a8debb9d11
tharun <godasutharun143@gmail.com>
2026-04-11 14:55:05 +0530

    url is updated with backend render url

--- Frontend/store-rating-app/src/api/api.jsx
+++ Frontend/store-rating-app/src/api/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:8089/api",
+  baseURL: process.env.REACT_APP_API_URL || "https://store-rating-app-x0zo.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
